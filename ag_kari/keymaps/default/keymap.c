4e69daf93573e7c254de8e03d9ad8f9950d4e392
kobakos <53651931+kobababa@users.noreply.github.com>
2021-04-05 15:01:18 +0900

    Update keymap.c

--- ag_kari/keymaps/default/keymap.c
+++ ag_kari/keymaps/default/keymap.c
@@ -28,11 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT( \
   //,--------+--------+---------+--------+---------+--------.   ,--------+---------+--------+---------+--------+--------.
-     KC_ESC , KC_Q   , KC_V    , KC_COMM, KC_DOT  , KC_SLSH,     KC_G   , KC_Y    , KC_D   , KC_J    , KC_P   , KC_MINS,
+     KC_ESC , KC_Q   , KC_W    , KC_E   , KC_R    , KC_T   ,     KC_Y   , KC_U    , KC_I   , KC_O    , KC_P   , KC_MINS,
   //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
-     KC_LCTL, KC_A   , KC_O    , KC_E   , KC_I    , KC_U   ,     KC_R   , KC_N    , KC_S   , KC_T    , KC_L   , KC_RCTL,
+     KC_LCTL, KC_A   , KC_S    , KC_D   , KC_F    , KC_G   ,     KC_H   , KC_J    , KC_K   , KC_L    , KC_   , KC_RCTL,
   //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
-     KC_TAB , KC_Z   , KC_X    , KC_C   , KC_W    , KC_F   ,     KC_B   , KC_H    , KC_K   , KC_M    , KC_COLN, _______,
+     KC_TAB , KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, _______,
   //`--------+--------+---------+--------+---------+--------/   \--------+---------+--------+---------+--------+--------'
               TT(3)  , KC_LSFT , KC_M_SPC,KC_DEL  , KC_ALT ,     KC_WIN , KC_BSCP , KC_M_ENT,KC_RSFT , TT(3)
   //                 `----------+--------+---------+--------'   `--------+---------+--------+---------'
